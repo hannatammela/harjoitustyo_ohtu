23adcabce39f99cbf4fb17634dc3a7d486c978f4
hannatammela <hanna.tammela@seamk.fi>
2026-01-21 08:49:54 +0200

    hello world sovellus luotu

---
+++ sovellus/src/main/java/harjoitustyo/sovellus/SovellusApplication.java
@@ -0,0 +1,13 @@
+package harjoitustyo.sovellus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SovellusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SovellusApplication.class, args);
+	}
+
+}
---
+++ sovellus/src/test/java/harjoitustyo/sovellus/SovellusApplicationTests.java
@@ -0,0 +1,13 @@
+package harjoitustyo.sovellus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SovellusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
